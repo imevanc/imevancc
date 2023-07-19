6db1bb4deb8579909954b48e0cb09cbc39254878
imevanc <evan.charalampidis@gmail.com>
2023-07-20 00:54:27 +0100

    header create container namespace export

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,7 @@ import { classNames } from "@/helpers/classNames";
 import { ContainerIF } from "@/app/interfaces/ContainerIF";
 
 const OuterContainer = forwardRef<HTMLDivElement, ContainerIF>(
-  ({ className, children, ...props }, ref) => (
+  ({ className, children, style, ...props }, ref) => (
     <div ref={ref} className={classNames("sm:px-8", className)} {...props}>
       <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
     </div>
@@ -22,7 +22,7 @@ const InnerContainer = forwardRef<HTMLDivElement, ContainerIF>(
   )
 );
 
-export const Container = forwardRef<HTMLDivElement, ContainerIF>(
+const Container = forwardRef<HTMLDivElement, ContainerIF>(
   ({ children, ...props }, ref) => (
     <OuterContainer ref={ref} {...props}>
       <InnerContainer ref={ref} {...props}>
@@ -32,5 +32,9 @@ export const Container = forwardRef<HTMLDivElement, ContainerIF>(
   )
 );
 
-Container.Outer = OuterContainer;
-Container.Inner = InnerContainer;
+const ContainerNamespace = Object.assign(Container, {
+  Outer: OuterContainer,
+  Inner: InnerContainer,
+});
+
+export { ContainerNamespace as Container };
