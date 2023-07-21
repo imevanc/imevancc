34082239755ba642f11a8ba705dd26c3b339d754
imevanc <evan.charalampidis@gmail.com>
2023-07-21 23:57:14 +0100

    layout create the about page to test the layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from "next/types";
+
+const Page: NextPage = () => <>asdfsadfasfdsf</>;
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 import type { NextPage } from "next/types";
-import { Header } from "@/components/Header";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-        </div>
-      </div>
-      <Header />
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
