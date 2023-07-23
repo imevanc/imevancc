e3914e1f96b05afb46c8382ffff63c4bc1640651
imevanc <evan.charalampidis@gmail.com>
2023-07-23 18:50:52 +0100

    card create the righticon

--- src/components/UtilityIcons.tsx
+++ src/components/UtilityIcons.tsx
@@ -52,3 +52,16 @@ export const MoonIcon: React.FC<{ className: string }> = ({ className }) => (
     />
   </svg>
 );
+
+export const ChevronRightIcon: React.FC<{ className: string }> = ({
+  className,
+}) => (
+  <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" className={className}>
+    <path
+      d="M6.75 5.75 9.25 8l-2.5 2.25"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
