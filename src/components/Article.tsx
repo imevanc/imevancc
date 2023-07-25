f2a94ecb374542b090cc5c820f4d63ecf77fc6ea
imevanc <evan.charalampidis@gmail.com>
2023-07-25 23:28:54 +0100

    articles create the article component and interface

---
+++ src/components/Article.tsx
@@ -0,0 +1,13 @@
+import { formatDate } from "@/helpers/formatDate";
+import { Card } from "./Card";
+
+export const Article: React.FC = ({ article }) => (
+  <Card as="article">
+    <Card.Title href={`/articles/${article.slug}`}>{article.title}</Card.Title>
+    <Card.Eyebrow as="time" dateTime={article.date} decorate>
+      {formatDate(article.date)}
+    </Card.Eyebrow>
+    <Card.Description>{article.description}</Card.Description>
+    <Card.Cta>Read article</Card.Cta>
+  </Card>
+);
