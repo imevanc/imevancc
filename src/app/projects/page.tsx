3e0bae350c0faa06dc241fe2d00399655728b1d7
imevanc <evan.charalampidis@gmail.com>
2023-08-02 23:26:51 +0100

    photography create the image grid

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -24,7 +24,12 @@ const Page: NextPage = () => (
       {projects.map((project: ProjectIF) => (
         <Card as="li" key={project.name}>
           <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-            <Image src={project.logo} alt="" className="h-8 w-8" unoptimized />
+            <Image
+              src={project.logo}
+              alt="logo"
+              className="h-8 w-8"
+              unoptimized
+            />
           </div>
           <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
             <Card.Link href={project.link.href}>{project.name}</Card.Link>
