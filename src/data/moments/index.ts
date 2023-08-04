6ac55939b18108d733fae14b90539f70a6248760
imevanc <evan.charalampidis@gmail.com>
2023-08-04 21:43:39 +0100

    moments integrate the sup photo into the moments page

--- src/data/moments/index.ts
+++ src/data/moments/index.ts
@@ -7,6 +7,16 @@ export const moments: Array<MomentIF> = [
     url: "/moments/helvellyn.jpg",
   },
   {
+    title: "Snowdon - 3,560ft",
+    description: "Breezy day in Yr Wyddfa",
+    url: "/moments/snowdon.jpg",
+  },
+  {
+    title: "Llyn Geirionydd - 630ft",
+    description: "SUP in February",
+    url: "/moments/llyn-geirionydd.jpg",
+  },
+  {
     title: "Mam Tor - 1,700ft",
     description: "Frosty day in autumn",
     url: "/moments/mamtor.jpg",
@@ -20,10 +30,5 @@ export const moments: Array<MomentIF> = [
     title: "Windemere - 128ft",
     description: "Lake cruise in Bowness",
     url: "/moments/windemere.jpg",
-  },
-  {
-    title: "Snowdon - 3,560ft",
-    description: "Breezy day in Yr Wyddfa",
-    url: "/moments/snowdon.jpg",
   },
 ];
