61f69eda405419151aab0996d4b3f073ffd7e652
imevanc <evan.charalampidis@gmail.com>
2023-08-05 20:59:49 +0100

    about add the tool imgs

---
+++ src/images/tools/index.ts
@@ -0,0 +1,31 @@
+import css3 from "./css3-plain.svg";
+import html5 from "./html5-plain.svg";
+import tailwindcss from "./tailwindcss-plain.svg";
+import javascript from "./javascript-plain.svg";
+import typescript from "./typescript-plain.svg";
+import aws from "./amazonwebservices-plain-wordmark.svg";
+import node from "./nodejs-plain-wordmark.svg";
+import react from "./react-original.svg";
+import gcp from "./googlecloud-original.svg";
+import postgres from "./postgresql-plain.svg";
+import jest from "./jest-plain.svg";
+import mui from "./materialui-plain.svg";
+import storybook from "./storybook-original.svg";
+import kubernetes from "./kubernetes-plain-wordmark.svg";
+
+export {
+  css3,
+  html5,
+  tailwindcss,
+  javascript,
+  typescript,
+  aws,
+  node,
+  react,
+  gcp,
+  postgres,
+  jest,
+  mui,
+  storybook,
+  kubernetes,
+};
